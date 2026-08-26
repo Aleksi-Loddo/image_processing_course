adcfde9d5ce5b44c7cfac36d10f9b40d2c3b53f0
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2026-08-26 20:04:50 +0300

    added  more answers

--- week1/test.py
+++ week1/test.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib
 
 
-print(np.__version__)
-print(Image.__version__)
-print(matplotlib.__version__)
+#print(np.__version__)
+#print(Image.__version__)
+#print(matplotlib.__version__)
 
 picture1 = "pictures/linnanmaa.jpg"
 
@@ -16,12 +16,20 @@ pixels=np.array(picture)
 #prints any pixels color value
 
 #ask user for a pixel to select
-pixel_x = int(input("Enter x coordinate: "))
-pixel_y = int(input("Enter y coordinate: "))
+#pixel_x = int(input("Enter x coordinate: "))
+#pixel_y = int(input("Enter y coordinate: "))
 #print color value of any pixel in a image
-print(pixels[pixel_x,pixel_y])
+#print(pixels[pixel_x,pixel_y])
 
 
 
 #print pixel value of upper left corner of image
-print(pixels[0,0])
+#print(pixels[0,0])
+
+width, height = picture.size
+
+print(f"width:{width},height:{height}")
+
+num_rows, num_cols,color_channels = pixels.shape
+
+print(f"the image is {num_rows} pixel height and {num_cols} pixel width ")
