d3c63583f9f288ccce3ae116108977ea2f6adbbc
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2026-08-26 19:42:54 +0300

    week 1 part   commit

---
+++ week1/test.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import numpy as np
+import matplotlib
+
+
+print(np.__version__)
+print(Image.__version__)
+print(matplotlib.__version__)
+
+picture1 = "pictures/linnanmaa.jpg"
+
+picture =Image.open(picture1)
+pixels=np.array(picture)
+
+
+#prints any pixels color value
+
+#ask user for a pixel to select
+pixel_x = int(input("Enter x coordinate: "))
+pixel_y = int(input("Enter y coordinate: "))
+#print color value of any pixel in a image
+print(pixels[pixel_x,pixel_y])
+
+
+
+#print pixel value of upper left corner of image
+print(pixels[0,0])
